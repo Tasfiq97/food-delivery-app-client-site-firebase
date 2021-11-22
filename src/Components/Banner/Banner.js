d4fdbce1731ac84d52c9a3df1364e911389335f2
Tasfiq97 <tasfiqulazad200@gmail.com>
2021-11-22 15:09:46 +0600

    new

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <h3>Welcome to</h3>
 <h1 className="lg:text-6xl font-semibold "> <span className="text-indigo-600 font-extrabold">Expressos</span></h1>
 <h1 className="lg:text-6xl font-semibold ">Delivery</h1>
-<p className="mt-2">Serving a lip-smacking food is our passion.</p>
+<p className="mt-2">Serving a lip-smacking food is our one and only passion.</p>
             <p className="mt-5">100 % Grass-fed beef patty, cheddar cheese, special sauce, tomato, pickles, lettuce, sesame seed bun ,all the food you want ,we deliver you perfectly,we are here to serve  </p>
            <HashLink to="#offers">
            <button style={{transition:"all 0.3s ease-in"}} className="p-4 pl-8 pr-8 text-2xl mt-5 border-1 bg-indigo-600 hover:bg-indigo-200 hover:text-black">Our Offers</button>
